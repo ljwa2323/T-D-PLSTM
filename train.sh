c8f8d26fcc60d4bb5839eda3f128a4ea0007920e
ljwa2323 <2111952576@qq.com>
2019-12-31 16:09:53 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python main.py --xs ./data/data3/Xs.csv --ma /home/ljw/project1/data/data3/mask.csv --de /home/ljw/project1/data/data3/deltat.csv --tp 10 --bign 3 --epoch 150 --mt 1 --timew 7
+python main.py --xs ./data/data3/Xs.csv --ma ./data/data3/mask.csv --de ./data/data3/deltat.csv --tp 10 --bign 3 --epoch 150 --mt 1 --timew 7
